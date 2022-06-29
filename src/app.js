cd3bfe1e5a584748c8297b2e37db089b8049587d
Kirill Lee <cyril.lee1997@gmail.com>
2022-06-29 13:33:07 +0300

    task done

--- src/app.js
+++ src/app.js
@@ -1,65 +1,58 @@
-const galleryItems = [
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg",
-    description: "Hokkaido Flower",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
-    description: "Container Haulage Freight",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",
-    description: "Aerial Beach View",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",
-    description: "Flower Blooms",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",
-    description: "Alpine Mountains",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",
-    description: "Mountain Lake Sailing",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
-    description: "Alpine Spring Meadows",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
-    description: "Nature Landscape",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
-    description: "Lighthouse Coast Sea",
-  },
-];
+import galleryItems from "./gallery-items.js";
+
+const imagesEl = document.querySelector(".gallery");
+const lightBox = document.querySelector(".lightbox");
+const lightBoxOverlay = document.querySelector(".lightbox__overlay");
+const lightBoxImage = document.querySelector(".lightbox__image");
+const closeButton = document.querySelector('[data-action="close-lightbox"]');
+
+galleryItems.map((e) =>
+  imagesEl.insertAdjacentHTML(
+    "afterbegin",
+    `<img srcset='${e.preview}' src='${e.original}'  alt='${e.description}' class='gallery__item gallery__image gallery__link'>`
+  )
+);
+
+imagesEl.addEventListener("click", onClickOpenLightBox);
+lightBox.addEventListener("click", onClickCloseLightBox);
+lightBox.addEventListener("click", onClickOverlayCloseLightBox);
+
+function onClickOpenLightBox(event) {
+  lightBox.classList.add("is-open");
+  lightBoxImage.setAttribute("src", `${event.target.src}`);
+  lightBoxImage.setAttribute("alt", `${event.target.alt}`);
+  window.addEventListener("keydown", onClickEscapeCloseLightBox);
+}
+function onClickCloseLightBox(event) {
+  if (event.target === closeButton) {
+    lightBox.classList.remove("is-open");
+    lightBoxImage.removeAttribute("src");
+    lightBoxImage.removeAttribute("alt");
+    window.removeEventListener("keydown", onClickEscapeCloseLightBox);
+  }
+}
+function onClickOverlayCloseLightBox(event) {
+  if (event.target === lightBoxOverlay) {
+    lightBox.classList.remove("is-open");
+    lightBoxImage.removeAttribute("src");
+    lightBoxImage.removeAttribute("alt");
+    window.addEventListener("keydown", onClickEscapeCloseLightBox);
+  }
+}
+function onClickEscapeCloseLightBox(event) {
+  if (event.code === "Escape") {
+    lightBox.classList.remove("is-open");
+    lightBoxImage.removeAttribute("src");
+    lightBoxImage.removeAttribute("alt");
+    window.addEventListener("keydown", onClickEscapeCloseLightBox);
+  }
+}
+
+// function onEscapeCloseLightBox(event) {
+//   console.log(lightBox);
+//   //   if (event.code === "Escape") {
+//   //     lightBox.classList.remove("is-open");
+//   //     lightBoxImage.removeAttribute("src");
+//   //     lightBoxImage.removeAttribute("alt");
+//   //   }
+// }
